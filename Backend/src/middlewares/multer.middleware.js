4b19556ac1453a1357496250661ba7aa36779be0
Aman Pratap Singh <120871048+Aman07122003@users.noreply.github.com>
2025-09-04 19:43:36 +0530

    multer updated

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,14 +1,21 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadPath = path.resolve("public/temp");
+
+// Ensure folder exists
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../public/temp")
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-export const upload = multer({
-  storage,
-});
+export const upload = multer({ storage });
